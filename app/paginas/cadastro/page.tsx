4c556ff7d7dfba583f705329307b6154d50005ed
KaueMazzonetto <mazzonettokaue@gmail.com>
2026-05-15 22:23:15 -0300

    Resolver problema do cadastro

--- app/paginas/cadastro/page.tsx
+++ app/paginas/cadastro/page.tsx
@@ -1,5 +1,7 @@
+// @ts-ignore: next/navigation types may be missing in this environment
 import { redirect } from "next/navigation";
 
 export default function CadastroLegacyPage() {
   redirect("/sistema/paginas/curriculos/novo");
+  return null;
 }
